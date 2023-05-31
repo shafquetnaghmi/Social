d172fa9a8dc8f285605346442956cd3c2604b60b
Shafquet Naghmi <naghmishafquet@gmail.com>
2023-06-01 00:04:42 +0530

    adding .env

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='naghmi.xyz@gmail.com'
-EMAIL_HOST_PASSWORD ='kydxyoxzqhyfmhss'
+EMAIL_HOST_USER =os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
 
 AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
